3255641739ca37562d757d4cc009c1fc959d51ec
Yarikonen <67190436+Yarikonen@users.noreply.github.com>
2023-03-26 22:55:02 +0300

    functions fixed with numpy

--- comp-math-lab2/functions.py
+++ comp-math-lab2/functions.py
@@ -3,9 +3,11 @@ import numpy as np
 def f1(x):
     return (x**2)
 def f2(x):
-    return( math.sin(x)*x + x**2)
+    return( np.sin(x)*x + x**2)
 def f3(x):
-    return(math.sqrt(x) + x*math.cos(x))
+    return(np.sqrt(x) + x*np.cos(x))
 def derivative(f, x):
+    
     return((f(x+0.0001)-f(x))/0.0001)
+
     
