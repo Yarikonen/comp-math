8fe081a90b1df59d1ce3cce849f8f5f4208c2221
Yarikonen <67190436+Yarikonen@users.noreply.github.com>
2023-03-30 12:37:44 +0300

    added visualization and functions logis

--- comp-math-lab2/functions.py
+++ comp-math-lab2/functions.py
@@ -1,5 +1,9 @@
-import math
+
 import numpy as np
+def system1(x,y):
+    return list([np.power(x,2) + np.power(y,2) -4, np.power(y,2) -x ])
+def system2(x):
+    return( np.array([0.1*x[0]**2+x[0]+0.2*x[1]**2-0.3,0.2*x[0]**2+x[1]+0.1*x[0]*x[1]-0.7]))
 def f1(x):
     return (x**2)
 def f2(x):
@@ -7,7 +11,40 @@ def f2(x):
 def f3(x):
     return(np.sqrt(x) + x*np.cos(x))
 def derivative(f, x):
-    
     return((f(x+0.0001)-f(x))/0.0001)
+def partial_derivative(f, x, i):
 
-    
+    y=np.array([x[j]+0.0001 if j==i else x[j] for j in range(len(x))])
+    return((f(y)-f(x))/0.0001)
+def jacobian(f,x):
+    return np.linalg.det(np.array([partial_derivative(f,x,i) for i in range(len(x))]))
+
+def check_if_monotonic(f,a,b):
+
+    tmp=derivative(f,a)
+    for i in range(a*10,b*10):
+        
+        if(derivative(f,i/10)*tmp<0):
+            return False
+    return True
+def check_if_not_equals_zero(f,a,b):
+
+    for i in range(a*10,b*10):
+        if(f(i)!=0):
+            return False
+    return True
+def get_function_by_index(index):
+    if(index==1):
+        return f1
+    if(index==2):
+        return f2
+    if(index==3):
+        return f3
+def get_system_by_index(index):
+    if(index==1):
+        return system1
+    if(index==2):
+        return system2
+def get_dimensions_of_system_by_index(index):
+    if(index==1):
+        return 2
--- comp-math-lab2/visualizer.py
+++ comp-math-lab2/visualizer.py
@@ -3,4 +3,13 @@ import numpy as np
 def draw_function(f,a,b):
     x= np.linspace(a,b,1000)
     plt.plot(x,f(x))
+    plt.grid()
+def draw_answ(x):
+    plt.scatter(x,0,color="red")
+    plt.show()
+def draw_implicit_function(f,a,b):
+    xrange = np.arange(a, b, 0.025)
+    yrange = np.arange(a, b, 0.025)
+    X,Y = np.meshgrid(xrange,yrange)
+    for i in f([X,Y]): plt.contour(X,Y,i,[0])
     plt.show()
